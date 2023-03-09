cb26e96448b2e074691f9f160271cf9d1d72ef63
shahid <shahdbest@gmail.com>
2023-03-09 15:52:18 +0530

    problem

---
+++ src/main/java/com/stackroute/oops/Product.java
@@ -0,0 +1,41 @@
+package com.stackroute.oops;
+
+/*
+    Product class stores the information about a single product.
+    It contains parameterized constructor and Getters/Setters
+ */
+public class Product {
+
+    public Product(int productCode, String name, double price, String category) {
+    }
+
+    public int getProductCode() {
+        return -1;
+    }
+
+    public void setProductCode(int productCode) {
+
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public void setName(String name) {
+    }
+
+    public double getPrice() {
+        return -1;
+    }
+
+    public void setPrice(double price) {
+    }
+
+    public String getCategory() {
+        return null;
+    }
+
+    public void setCategory(String category) {
+
+    }
+}
---
+++ src/main/java/com/stackroute/oops/ProductService.java
@@ -0,0 +1,28 @@
+package com.stackroute.oops;
+
+/*
+    Class for Analyzing the products present in ProductRepository
+ */
+public class ProductService {
+
+    /*
+        Returns the name of the product given the productCode
+     */
+    public String findProductNameByCode(int productCode) {
+        return "";
+    }
+
+    /*
+        Returns the Product with maximum price in a given category
+     */
+    public Product findMaxPriceProductInCategory(String category) {
+        return null;
+    }
+
+    /*
+        Returns a array of products for a given category
+     */
+    public Product[] getProductsByCategory(String category) {
+        return new Product[]{};
+    }
+}
